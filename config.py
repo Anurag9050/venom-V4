eae990096c9a61f4a57fcf9fa149ac081fcc612c
Anurag9050 <Pm9671010408@gmail.com>
2024-07-19 16:13:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7498264205:AAEKTrOCVUlkMpnGetnBF8iQdEBsvDar0CA")
+BOT_TOKEN=("7275128084:AAGD-Iqyi6XrMbWMjTBuzq_p1VXc49ByBLs")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("Sabka Papa")
